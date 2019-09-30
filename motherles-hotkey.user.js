2d01cc9627da938b0cc6f6a0c63487b6bb6b166a
Madagambada <47931256+Madagambada@users.noreply.github.com>
2019-09-30 18:27:45 +0200

    Update motherles-hotkey.user.js

--- motherles-hotkey.user.js
+++ motherles-hotkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        motherles-hotkey
-// @version     0.0.0.8a
+// @version     0.0.0.9a
 // @include     https://motherless.com*
 // @description Better motherles navigation
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
