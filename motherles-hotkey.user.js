e08860380ae0cb30f6cef630af0710f0406a8b29
Madagambada <47931256+Madagambada@users.noreply.github.com>
2019-10-01 19:20:15 +0200

    Update motherles-hotkey.user.js

--- motherles-hotkey.user.js
+++ motherles-hotkey.user.js
@@ -8,29 +8,29 @@
 // @downloadURL https://github.com/Madagambada/userscripts/raw/master/motherles-hotkey.user.js
 // ==/UserScript==
 
-if (RegExp('motherless.com/*/*').test(window.location.href))  {
-var NGaI = $("a:contains('Next →')");
-var PGaI = $("a:contains('← Previous')");
-(function(){
-document.addEventListener('keydown', function(e) {
-  if (e.keyCode == 39 || e.keyCode == 68) {
-   window.location = NGaI[0].href;
-  }
-  else if (e.keyCode == 37 || e.keyCode == 65) {
-   window.location = PGaI[0].href;
-  }
-}, false);
-})();}
+if (RegExp('motherless.com/*/*').test(window.location.href)) {
+ var NGaI = $("a:contains('Next →')");
+ var PGaI = $("a:contains('← Previous')");
+ (function() {
+  document.addEventListener('keydown', function(e) {
+   if (e.keyCode == 39 || e.keyCode == 68) {
+    window.location = NGaI[0].href;
+   } else if (e.keyCode == 37 || e.keyCode == 65) {
+    window.location = PGaI[0].href;
+   }
+  }, false);
+ })();
+}
 
-if (RegExp('motherless.com/g/*/*').test(window.location.href))  {
-var NGr = $("a:contains('Next Group')");
-(function(){
-document.addEventListener('keydown', function(e) {
-  if (e.keyCode == 39 || e.keyCode == 68) {
-   window.location = NGr[0].href;
-  }
-  else if (e.keyCode == 37 || e.keyCode == 65) {
-   window.history.back();
-  }
-}, false);
-})();}
+if (RegExp('motherless.com/g/*/*').test(window.location.href)) {
+ var NGr = $("a:contains('Next Group')");
+ (function() {
+  document.addEventListener('keydown', function(e) {
+   if (e.keyCode == 39 || e.keyCode == 68) {
+    window.location = NGr[0].href;
+   } else if (e.keyCode == 37 || e.keyCode == 65) {
+    window.history.back();
+   }
+  }, false);
+ })();
+}
