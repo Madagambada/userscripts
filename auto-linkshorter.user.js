29bad479cb07296524dfd41ed8347da501126cef
Madagambada <47931256+Madagambada@users.noreply.github.com>
2020-01-03 21:12:31 +0100

    Update auto-linkshorter.user.js

--- auto-linkshorter.user.js
+++ auto-linkshorter.user.js
@@ -1,59 +1,80 @@
 // ==UserScript==
 // @name         auto-linkshorter
-// @version      0.0.0.2g
-// @match        https://spiin.xyz/*
-// @match        https://cpmlink.net/go/*
-// @match        https://ouo.press/*
-// @match        https://bcvc.live/*
-// @match        http*://gusimp.net/*/*
+// @version      0.0.0.3k
+// @include        http://*
+// @include        https://*
 // @description  auto-linkshorter
 // @author       Madagambada
 // @updateURL    https://github.com/Madagambada/userscripts/raw/master/auto-linkshorter.user.js
 // @downloadURL  https://github.com/Madagambada/userscripts/raw/master/auto-linkshorter.user.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // ==/UserScript==
+
 (function() {
-if (RegExp('cpmlink.net/*').test(window.location.href)) {
-setTimeout(cpmlink, 3000);
-}
-if (RegExp('gusimp.net/*').test(window.location.href)) {
-setTimeout(adfly, 9500);
-}
-if (RegExp('ouo.press/*').test(window.location.href)) {
-setTimeout(ouo, 4000);
-}
-if (RegExp('bcvc.live/*').test(window.location.href)) {
-setTimeout(bcvc, 1000);
-}
-if (RegExp('spiin.xyz/*').test(window.location.href)) {
-if ($(".link_ready")[0]){
-setTimeout(exeio, 8000);
-} else {
-setTimeout(exeio, 500);
-}
-}
+ if (RegExp('cpmlink.net/*').test(window.location.href)) {
+  setTimeout(cpmlink, 3000);
+ }
+ if (RegExp('gusimp.net/ad*').test(window.location.href)) {
+  console.log("test")
+ } else if (RegExp('gusimp.net/*').test(window.location.href)) {
+  setTimeout(adfly, 9500);
+ }
+if ($("title:contains('Free URL shorten service - ouo.press')").length) {
+  setTimeout(ouo, 4000);
+ }
 
-
+if ($("title:contains('exe.io')").length) {
+  if ($(".link_ready")[0]) {
+   setTimeout(exeio, 8000);
+  } else {
+   setTimeout(exeio, 500);
+  }
+ }
+ if ($("title:contains('GPlinks')").length) {
+  if ($("span:contains(' SUBSCRIBE ')").length) {
+  } else if ($("h4:contains('Your link is almost ready.')").length) {
+   if ($('#countdown').hover().length = 1) {
+    setTimeout(function() {
+     setTimeout(gplinks, 500);
+    }, 16500)
+   }
+  }
+  else {
+   setTimeout(gplinks, 500);
+  }
+ }
 })();
 
 function cpmlink() {
-    var a_href = $("a:contains('Get Link')").attr('href');
-    window.location = a_href
+ var a_href = $("a:contains('Get Link')").attr('href');
+ window.location = a_href
 }
+
 function adfly() {
-    var a_href = $("a[class*='mwButton']").attr('href');
-    window.location = a_href
+ var a_href = $("a[class*='mwButton']").attr('href');
+ window.location = a_href
 }
+
 function ouo() {
-    $("#btn-main").click();
+ $("#btn-main").click();
 }
-function bcvc() {
-    //window.location = document.title
-}
+
 function exeio() {
-    if ($(".link_ready")[0]){
-        $('.link_ready:first').mouseenter().mouseleave();
-        window.location = $(".procced").children().attr('href')
-    }
-    $("#invisibleCaptchaShortlink").click();
+ if ($(".link_ready")[0]) {
+  $('.link_ready:first').mouseenter().mouseleave();
+  window.location = $(".procced").children().attr('href')
+ }
+ $("#invisibleCaptchaShortlink").click();
+
+}
+
+function gplinks() {
+    console.log("test2")
+
+ if ($("h4:contains('Your link is almost ready.')").length) {
+  var a_href = $("a[class*='btn btn-success btn-lg get-link']").attr('href');
+  window.location = a_href
+ } else {
+  $("button[class*='btn btn-primary']").click();
+ }
 }
