a0fc372b68917d72c2939e8e95b520c9478ef278
Madagambada <47931256+Madagambada@users.noreply.github.com>
2020-04-06 06:17:15 +0200

    hotkey on catalog

--- nhentai-hotkey.user.js
+++ nhentai-hotkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        nhentai-hotkey
-// @version     0.0.0.1z
-// @include     https://nhentai.net/g/*
+// @version     0.0.0.2h
+// @include     https://nhentai.net/*
 // @description nhentai hotkey
 // @author      Madagambada
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
@@ -10,14 +10,20 @@
 // ==/UserScript==
 
 //hotkey
+
 (function() {
  document.addEventListener('keydown', function(e) {
    if (e.keyCode == 96) {
        if ($("a:contains('Back to gallery')").length) {
            window.location = $("a:contains('Back to gallery')").attr('href');
-       } else {
+       } else if ($('#cover').length) {
            window.location = window.location.href+"1";
        }
+   }
+   if (e.keyCode == 37 && $("a[class*='previous']").length && $("a:contains('Back to gallery')").length == 0) {
+       window.location = $("a[class*='previous']").attr('href')
+   } else if (e.keyCode == 39 && $("a[class*='next']").length && $("a:contains('Back to gallery')").length == 0) {
+       window.location = $("a[class*='next']").attr('href')
    }
   }, false);
 })();
