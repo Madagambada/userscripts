9c0660fc8ede99ecb65ca069c409d8f56c5863e2
Madagambada <47931256+Madagambada@users.noreply.github.com>
2020-04-22 06:27:32 +0200

    Update mangakakalot-hotkey.user.js

--- mangakakalot-hotkey.user.js
+++ mangakakalot-hotkey.user.js
@@ -1,24 +1,29 @@
 // ==UserScript==
 // @name         mangakakalot hotkey
-// @version      0.0.0.3k
+// @version      0.0.1.1u
 // @description  mangakakalot hotkey
 // @author       Madagambada
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @updateURL    https://github.com/Madagambada/userscripts/raw/master/mangakakalot-hotkey.user.js
 // @downloadURL  https://github.com/Madagambada/userscripts/raw/master/mangakakalot-hotkey.user.js
-// @match        https://mangakakalot.com/chapter/*/*
-// @match        https://manganelo.com/chapter/*/*
+// @match        https://manganelo.com/*
+// @match        https://mangakakalot.com/*
 // @grant        none
 // ==/UserScript==
 
- var next = $("a:contains('NEXT CHAPTER')");
- var prev = $("a:contains('PREV CHAPTER')");
+if ($(".group_qty").length){
+ var next = $(".page_select").next()[0].href;
+ var prev = $(".page_select").prev()[0].href;
+} else {
+ var next = $("a:contains('NEXT CHAPTER')")[0].href;
+ var prev = $("a:contains('PREV CHAPTER')")[0].href;
+}
  (function() {
   document.addEventListener('keydown', function(e) {
    if (e.keyCode == 39 || e.keyCode == 68) {
-    window.location = next[0].href;
+    window.location = next;
    } else if (e.keyCode == 37 || e.keyCode == 65) {
-    window.location = prev[0].href;
+    window.location = prev;
    }
   }, false);
  })();
