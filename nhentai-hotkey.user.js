712bf01eb8d5e6f6dca640fc16ffff169fa1e373
Madagambada <47931256+Madagambada@users.noreply.github.com>
2020-06-09 14:57:08 +0200

    works with the new update

--- nhentai-hotkey.user.js
+++ nhentai-hotkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        nhentai-hotkey
-// @version     0.0.0.2h
+// @version     0.0.0.3i
 // @include     https://nhentai.net/*
 // @description nhentai hotkey
 // @author      Madagambada
@@ -14,15 +14,15 @@
 (function() {
  document.addEventListener('keydown', function(e) {
    if (e.keyCode == 96) {
-       if ($("a:contains('Back to gallery')").length) {
-           window.location = $("a:contains('Back to gallery')").attr('href');
+       if ($("a[class*='go-back']").length) {
+           window.location = $("a[class*='go-back']").attr('href');
        } else if ($('#cover').length) {
            window.location = window.location.href+"1";
        }
    }
-   if (e.keyCode == 37 && $("a[class*='previous']").length && $("a:contains('Back to gallery')").length == 0) {
+   if (e.keyCode == 37 && $("a[class*='previous']").length && $("a[class*='go-back']").length == 0) {
        window.location = $("a[class*='previous']").attr('href')
-   } else if (e.keyCode == 39 && $("a[class*='next']").length && $("a:contains('Back to gallery')").length == 0) {
+   } else if (e.keyCode == 39 && $("a[class*='next']").length && $("a[class*='go-back']").length == 0) {
        window.location = $("a[class*='next']").attr('href')
    }
   }, false);
