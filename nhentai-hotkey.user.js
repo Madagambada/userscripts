79d1fd34cdb6ca11d05773d7bcbb774d2520107a
Madagambada <47931256+Madagambada@users.noreply.github.com>
2020-10-23 02:48:37 +0200

    comment fix

--- nhentai-hotkey.user.js
+++ nhentai-hotkey.user.js
@@ -1,23 +1,26 @@
 // ==UserScript==
 // @name        nhentai-hotkey
-// @version     0.0.0.3i
+// @version     1.0.0.3i
 // @include     https://nhentai.net/*
 // @description nhentai hotkey
 // @author      Madagambada
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require     https://code.jquery.com/jquery-3.5.1.min.js
 // @updateURL   https://github.com/Madagambada/userscripts/raw/master/nhentai-hotkey.user.js
 // @downloadURL https://github.com/Madagambada/userscripts/raw/master/nhentai-hotkey.user.js
 // ==/UserScript==
 
 //hotkey
-
 (function() {
  document.addEventListener('keydown', function(e) {
    if (e.keyCode == 96) {
        if ($("a[class*='go-back']").length) {
            window.location = $("a[class*='go-back']").attr('href');
        } else if ($('#cover').length) {
-           window.location = window.location.href+"1";
+           if (window.location.href.search("#comment") > 0) {
+               window.location = window.location.href.split('#')[0]+"1";
+           } else {
+               window.location = window.location.href+"1";
+           }
        }
    }
    if (e.keyCode == 37 && $("a[class*='previous']").length && $("a[class*='go-back']").length == 0) {
