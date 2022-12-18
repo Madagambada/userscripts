3a6fe5b2ab0d8282621e0e71063912013ca5f6f9
Madagambada <47931256+Madagambada@users.noreply.github.com>
2022-12-18 13:26:13 +0100

    Update liveomg-remove-sites.user.js

--- liveomg-remove-sites.user.js
+++ liveomg-remove-sites.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         liveomg-remove-sites
-// @version      1.0.4
+// @version      1.0.5
 // @description  Removes specific sites
 // @author       Madagambada
 // @namespace    https://github.com/Madagambada
@@ -9,12 +9,28 @@
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @include      http://liveomg.com/*
 // @match        https://liveomg.co.uk/*
-// @grant        none
+// @run-at       document-start
+// @grant        GM_addStyle
 // ==/UserScript==
 
 setTimeout(function(){
    window.location.reload(1);
 }, 10000);
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+addGlobalStyle('.host {font-size:14px;color: #ECEFF4; }');
+addGlobalStyle('.viewers {font-size: 16px; color: #ECEFF4; }');
+addGlobalStyle('.promo {color: #999; background: #4C566A; padding: 12px 40px;}');
+addGlobalStyle('body { font-family: arial,helvetica,sans-serif; color: #000;  background-color: #3B4252;  text-align: center;}');
 
 (function() {
     var Array = $("span:contains('crazycash.tv')").map(function() {
@@ -35,6 +51,12 @@ setTimeout(function(){
     var Array6 = $("span:contains('tango.me')").map(function() {
         return $(this);
     }).get();
+    var Array7 = $("span:contains('viewhub.show')").map(function() {
+        return $(this);
+    }).get();
+    var Array8 = $("span:contains('afreecatv.com')").map(function() {
+        return $(this);
+    }).get();
     //console.log(Array);
     for (var i = 0; i < Array.length; i++) {
         Array[i].parent().parent().remove()
@@ -53,5 +75,11 @@ setTimeout(function(){
     }
     for (var i = 0; i < Array6.length; i++) {
         Array6[i].parent().parent().remove()
+    }
+    for (var i = 0; i < Array7.length; i++) {
+        Array7[i].parent().parent().remove()
+    }
+    for (var i = 0; i < Array8.length; i++) {
+        Array8[i].parent().parent().remove()
     }
 })();
